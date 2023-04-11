c563a82cda02c01477422a36656cbaa0d0d3034c
elkprostoelk <novikovmisha138138@gmail.com>
2023-04-11 19:56:26 +0300

    added tabs for science work page

--- src/app/components/user-profile-science-work/user-profile-science-work.component.ts
+++ src/app/components/user-profile-science-work/user-profile-science-work.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile-science-work.component.css']
 })
 export class UserProfileScienceWorkComponent {
+  currentTab = 'tab1';
 
+  tabs = [
+    {id: 'tab1', title: 'Tab 1', component: 'tab1-component'},
+    {id: 'tab2', title: 'Tab 2', component: 'tab2-component'},
+    {id: 'tab3', title: 'Tab 3', component: 'tab3-component'},
+    {id: 'tab4', title: 'Tab 4', component: 'tab4-component'}
+  ];
+
+  onNavChange(event: any) {
+    this.currentTab = event;
+  }
 }
